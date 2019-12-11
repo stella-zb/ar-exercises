7d25f9a9088231a09245cd9f3d968aba2f56d707
stella-zb <upperdaystudio@gmail.com>
2019-12-10 18:49:52 -0800

    one to many assoication between class

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jordyn", last_name: "Daniel", hourly_rate: 50)
+@store1.employees.create(first_name: "Shahid", last_name: "Hawes", hourly_rate: 60)
+@store2.employees.create(first_name: "Della", last_name: "Hulme", hourly_rate: 40)
+@store2.employees.create(first_name: "Amandeep", last_name: "Nicohole", hourly_rate: 60)
+@store2.employees.create(first_name: "Phyllis", last_name: "Roche", hourly_rate: 50)
