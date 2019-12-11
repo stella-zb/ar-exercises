d20e86b6eb08ab9c9f8101c00225fccb3234b2f5
stella-zb <upperdaystudio@gmail.com>
2019-12-10 17:58:39 -0800

    fetch the data from database

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for store in @mens_stores
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true).select{ |store| store.annual_revenue < 1000000 }
+
+for store in @womens_stores
+  puts store.name
+  puts store.annual_revenue
+end
